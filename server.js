02254db98e05fad61518835d80354bc4c280343c
oscarhandsome <altair5555@gmail.com>
2023-04-03 16:10:06 +0600

    added heroku config

--- server.js
+++ server.js
@@ -40,4 +40,12 @@ process.on('unhandledRejection', err => {
   });
 });
 
+process.on('SIGTERM', () => {
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('💥 Process terminated!');
+    // process.exit(1);
+  });
+});
+
 // console.log(x);
